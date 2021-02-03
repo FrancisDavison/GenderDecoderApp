f64e1d318af63b63250ddca88539d3a5f4186351
FrancisDavison <francis.nj.davison@gmail.com>
2021-02-03 13:11:39 +0000

    added spring initializr files

---
+++ genderdecoder/src/main/java/com/example/genderdecoder/GenderdecoderApplication.java
@@ -0,0 +1,13 @@
+package com.example.genderdecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderdecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderdecoderApplication.class, args);
+	}
+
+}
